c829b117b540eddb0670ac94479feea3467bfcfc
okhomyk <oleh.khomyk@gmail.com>
2026-04-13 14:44:15 +0300

    Added pagination, synced with url query params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,21 +2,26 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
-    if (term.trim()) {
+
+    if (params.has('page'))
+      params.set('page', '1');
+
+    if (term.trim())
       params.set('query', term);
-    } else {
+    else
       params.delete('query');
-    }
+
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
